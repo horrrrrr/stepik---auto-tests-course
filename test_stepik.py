9a254466d528c3677a822f1c3675d9c09e681b14
horrrrrr <gera.prikol@yandex.ru>
2019-10-07 19:22:13 +0700

    07.10

--- test_stepik.py
+++ test_stepik.py
@@ -372,7 +372,21 @@ def test24_step_8 ():
         time.sleep(10)
         driver.quit()
 
-50
+
+# подставляем переменные в текст ошибки с помощью f и {}
+def test_input_text(expected_result, actual_result):
+    assert expected_result == actual_result, f"expected {expected_result}, got {actual_result}"
+
+
+
+
+# s = 'My Name is Julia'
+#
+# if 'Name' in s:
+#     print('Substring found')
+#
+def test_substring(full_string, substring):
+    assert substring in full_string, f"expected '{substring}' to be substring of '{full_string}'"
 
 
 
